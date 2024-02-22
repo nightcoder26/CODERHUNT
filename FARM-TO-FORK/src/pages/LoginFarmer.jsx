0c34dfc597e1c89d42ed262a38f6525857ccb287
Knight <77844703+nightcoder26@users.noreply.github.com>
2024-02-22 23:54:33 +0530

    Update LoginFarmer.jsx

--- FARM-TO-FORK/src/pages/LoginFarmer.jsx
+++ FARM-TO-FORK/src/pages/LoginFarmer.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import { useEffect } from "react";
+import axios from "axios";
+import { useState } from "react";
 
 const LoginFarmer = () => {
-  return <div>LoginFarmer</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("Farmer");
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:3000/login", {
+      username: username,
+      password: password,
+      role: role,
+    });
+    // console.log(username, password, role);
+  };
+  return (
+    <>
+      <div className="login">
+        <h1>Login as Farmer</h1>
+        <form onSubmit={handleLoginSubmit}>
+          <input
+            type="text"
+            placeholder="Username"
+            required
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <input type="text" value="Farmer" hidden />
+          <button type="submit">Login</button>
+          <p className="message">
+            Not registered? <a href="/register">Create an account</a>
+          </p>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default LoginFarmer;
